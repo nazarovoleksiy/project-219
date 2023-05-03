efc4a78c730acbf964d2890d9e35664205487897
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-03 13:15:06 +0300

    221. Allowing the Selection of Items in the List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,22 +1,33 @@
-import {
-  Component,
-  OnInit,
-} from '@angular/core';
-
-import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+
+
 import {NgForm} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {Component, OnDestroy, OnInit,} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editItemIndex: number;
 
   constructor(private slService: ShoppingListService) { }
 
+  ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+  }
+
   ngOnInit() {
+    this.subscription = this.slService.startedEditing
+      .subscribe((index: number) => {
+        this.editItemIndex = index;
+        this.editMode = true;
+      })
   }
 
   onAddItem(form: NgForm) {
