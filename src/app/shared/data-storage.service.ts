a3cf65721b5e4422f86d1f57eb90c03240f29eb6
Nazarov Oleksiy <sonynazarov@gmail.com>
2023-05-08 12:21:38 +0300

    282. Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+import { RecipeService } from "../recipes/recipe.service";
+
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
   constructor(
     private http: HttpClient,
+    private recipesService: RecipeService,
   ) {};
 
+  storeRecipes(){
+    const recipes = this.recipesService.getRecipes();
+    this.http.put('https://ng-course-recipe-book-nazarov-default-rtdb.firebaseio.com/recipes.json', recipes)
+      .subscribe(
+        response => {
+          console.log(response)
+        }
+      );
+  }
 
 }
