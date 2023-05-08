92cb78f21f2a57583cf9a7655316164283011ed9
Nazarov Oleksiy <sonynazarov@gmail.com>
2023-05-08 13:10:09 +0300

    284. Transforming Response Data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import {map} from "rxjs/operators";
 
 
 @Injectable({providedIn: 'root'})
@@ -24,6 +25,11 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://ng-course-recipe-book-nazarov-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }))
       .subscribe(
         recipes => {
           this.recipesService.setRecipes(recipes);
