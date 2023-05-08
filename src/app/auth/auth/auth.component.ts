914627ba83815e003fd0ff5d15f979aed5647445
Nazarov Oleksiy <sonynazarov@gmail.com>
2023-05-08 15:44:01 +0300

    295. Sending the Signup Request

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -7,19 +8,40 @@ import { NgForm } from "@angular/forms";
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService
+  ) {}
   isLoginMode = true;
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
   }
 
-  constructor() { }
-
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+
+    } else {
+      this.authService.signup(email, password)
+        .subscribe(
+          resData => {
+            console.log(resData);
+          },
+          error => {
+            console.log(error)
+          }
+        );
+    }
+
     form.reset();
   }
 }
