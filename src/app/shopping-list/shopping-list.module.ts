f56d6e445036f27d45c0d11c8eed1aaeaed2438d
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-05-17 12:07:26 +0300

    { path: shopping-list, component: ShoppingListComponent },

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+
+import {ShoppingListComponent} from "./shopping-list.component";
+import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'shopping-list', component: ShoppingListComponent },
+    ])
+  ],
+
+})
+export class ShoppingListModule {
+
+}
